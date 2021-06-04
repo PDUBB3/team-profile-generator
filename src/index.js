75d6bc7e9bf652180001a1b113a92a9e8c588589
Pamela <pamela_bhopal@hotmail.co.uk>
2021-06-04 13:30:17 +0100

    add engineer and intern questions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const inquirer = require("inquirer");
 
 const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
 
 const employees = [];
 
@@ -40,7 +42,102 @@ const addManager = async () => {
   employees.push(manager);
 };
 
-const addEmployees = async () => {};
+const addEngineer = async () => {
+  const engineerQuestions = [
+    {
+      name: "name",
+      type: "input",
+      message: "Please enter engineer's name:",
+    },
+    {
+      name: "email",
+      type: "input",
+      message: "Please enter engineer's email address:",
+    },
+    {
+      name: "id",
+      type: "input",
+      message: "Please enter engineer's employee ID:",
+    },
+    {
+      name: "github",
+      type: "input",
+      message: "Please enter engineer's github username:",
+    },
+  ];
+
+  const answers = await inquirer.prompt(engineerQuestions);
+
+  const engineer = new Engineer(
+    answers.id,
+    answers.name,
+    answers.email,
+    answers.github
+  );
+
+  employees.push(engineer);
+};
+
+const addIntern = async () => {
+  const internQuestions = [
+    {
+      name: "name",
+      type: "input",
+      message: "Please enter intern's name:",
+    },
+    {
+      name: "email",
+      type: "input",
+      message: "Please enter intern's email address:",
+    },
+    {
+      name: "id",
+      type: "input",
+      message: "Please enter intern's employee ID:",
+    },
+    {
+      name: "school",
+      type: "input",
+      message: "Please enter intern's school:",
+    },
+  ];
+
+  const answers = await inquirer.prompt(internQuestions);
+
+  const intern = new Intern(
+    answers.id,
+    answers.name,
+    answers.email,
+    answers.school
+  );
+
+  employees.push(intern);
+};
+
+const addEmployees = async () => {
+  let inProgress = true;
+  while (inProgress) {
+    const employeeQuestion = {
+      name: "employee",
+      type: "list",
+      choices: ["Engineer", "Intern", "Exit"],
+      message: "Please select the employee you would like to add:",
+    };
+    const answers = await inquirer.prompt(employeeQuestion);
+
+    if (answers.employee === "Engineer") {
+      await addEngineer();
+    }
+
+    if (answers.employee === "Intern") {
+      await addIntern();
+    }
+
+    if (answers.employee === "Exit") {
+      inProgress = false;
+    }
+  }
+};
 
 const generateHtml = async () => {};
 
@@ -48,6 +145,8 @@ const writeToFile = async () => {};
 
 const init = async () => {
   await addManager();
+  await addEmployees();
+  console.log(employees);
 };
 
 init();
